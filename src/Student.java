dacd016dbadee8b8d9a1034e6dc5776c1ead0b7d
jahnavi0904 <jahnavikatta2000@gmail.com>
2023-06-01 12:42:36 -0500

    done

--- src/Student.java
+++ src/Student.java
@@ -1,17 +1,17 @@
 public class Student {
-    String Name;
+        String Name;
 
-    public void setName(String Name) {
-        this.Name = Name;
-    }
+        public void setName(String Name) {
+            this.Name = Name;
+        }
 
-    public String getName() {
-        return Name;
-    }
+        public String getName() {
+            return Name;
+        }
 
-    public static void main(String[] args) {
-        Student J = new Student();
-        J.setName("jahnavi");
-        System.out.println(J.getName());
+        public static void main(String[] args) {
+            Student S = new Student();
+            S.setName("jahnavi");
+            System.out.println(S.getName());
+        }
     }
-}
