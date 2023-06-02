7c11c87d0e5143e8ec610a26e92b1b207ddb9d4d
Jahnavi0904 <129913683+Jahnavi0904@users.noreply.github.com>
2023-06-02 11:45:48 -0500

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,12 @@
+public abstract class Department {
+
+
+    public abstract int getDepartmentsize();
+
+}
+class Section extends Department {
+    public int getDepartmentsize() {
+        return 20;
+    }
+
+}
