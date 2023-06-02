dadad398cbd2709f8207e125b52c00922d8185ff
Jahnavi0904 <129913683+Jahnavi0904@users.noreply.github.com>
2023-06-02 12:34:29 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Section count=new Section();
-        int departmentsize=count.getDepartmentsize();
-        System.out.println("departmentsize:"+departmentsize);
-
-    }
-}
